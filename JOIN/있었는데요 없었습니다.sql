ecc7ba655768fa88f3218a03258a092251534344
kyeong-hyeok <minkh4227@naver.com>
2023-03-08 22:16:44 +0900

    [ADD] 있었는데요 없었습니다

---
+++ JOIN/있었는데요 없었습니다.sql
@@ -0,0 +1,14 @@
+-- 관리자의 실수로 일부 동물의 입양일이 잘못 입력되었습니다. 
+-- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 
+-- 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
+
+-- 일반 조인 1
+SELECT O.ANIMAL_ID, O.NAME FROM ANIMAL_OUTS O
+INNER JOIN ANIMAL_INS I ON O.ANIMAL_ID = I.ANIMAL_ID
+WHERE O.DATETIME < I.DATETIME
+ORDER BY I.DATETIME
+
+-- 일반 조인 2
+SELECT O.ANIMAL_ID, O.NAME FROM ANIMAL_OUTS O, ANIMAL_INS I
+WHERE O.ANIMAL_ID = I.ANIMAL_ID AND O.DATETIME < I.DATETIME
+ORDER BY I.DATETIME
