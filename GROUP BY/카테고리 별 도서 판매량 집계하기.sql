e175a874213f75aa05ec40ee0cdf49de8c4307e3
kyeong-hyeok <minkh4227@naver.com>
2023-04-04 23:19:30 +0900

    [ADD] 카테고리 별 도서 판매량 집계하기

---
+++ GROUP BY/카테고리 별 도서 판매량 집계하기.sql
@@ -0,0 +1,9 @@
+
+-- 2022년 1월의 카테고리 별 도서 판매량을 합산하고, 카테고리(CATEGORY), 총 판매량(TOTAL_SALES) 리스트를 출력하는 SQL문을 작성해주세요.
+-- 결과는 카테고리명을 기준으로 오름차순 정렬해주세요.
+
+SELECT CATEGORY, SUM(SALES) AS TOTAL_SALES
+FROM BOOK B, BOOK_SALES BS
+WHERE B.BOOK_ID = BS.BOOK_ID AND SALES_DATE LIKE '2022-01%'
+GROUP BY CATEGORY
+ORDER BY CATEGORY
